15166fec5d9e84c1e6ac6815c39eade8c4e65679
perchik_air <1919ars@gmail.com>
2022-06-06 16:15:25 +0300

    first

---
+++ GLOBALS.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import matplotlib
+import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
+import numpy as np
+import random
+import math
+import copy
+from collections import OrderedDict
+from scipy.spatial.distance import cdist
+import abc
+import neptune.new as neptune
+
+import torch
+import torchvision
+import torchvision.transforms as T
+from torchvision.io import ImageReadMode
+
+import itertools
+from itertools import combinations, permutations
+from collections import defaultdict, Counter
+from pprint import pprint
+from datetime import datetime
+import time
+import json
---
+++ README.md
@@ -0,0 +1,8 @@
+# DCOP_MST Simulator (version 4)
+
+
+## Credits
+
+- [github | DCOP_MST Simulator (Version 3)](https://github.com/Arseni1919/dcop_simulator_3)
+- [github | MAPF Simulator](https://github.com/Arseni1919/MAPF_Simulator)
+- [github | Multi-Agent System Simulator](https://github.com/Arseni1919/mas_simulator_1)
