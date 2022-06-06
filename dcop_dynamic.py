fdd75c4e70a6fd38bce9989c232705365fbfa0d2
perchik_air <1919ars@gmail.com>
2022-06-06 18:20:41 +0300

    more

--- dcop_dynamic.py
+++ dcop_dynamic.py
@@ -2,9 +2,17 @@ from GLOBALS import *
 from nodes import TargetNode, AgentNode
 
 
+def create_dynamic_dcop_setting(lifespan=100):
+    pass
+
+
 def main():
     pass
 
 
 if __name__ == '__main__':
+    LIFE_SPAN = 100
+    N_TARGETS = 10
+    N_AGENTS = 30
+
     main()
